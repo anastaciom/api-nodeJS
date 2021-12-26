e556d729cc14200ea4235236fddfb34d74c3dc56
Anastacio Menezes <anastaciomenezes@outlook.com>
2021-12-25 21:53:09 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+//INITIAL CONFIG
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const users = require('./src/users/users.json')
+const todolist = require('./src/todolist/todolist.json')
+
+app.use(express.json())
+app.use(cors())
+
+//PORT
+
+const PORT = 8080
+
+//ROUTES
+
+app.get('/users', (req, res)=>{
+    res.json(users)
+})
+app.get('/todolist', (req, res)=>{
+    res.json(todolist)
+})
+
+
+//INITIALIZING THE SERVER
+
+app.listen(PORT, (err) => {
+    if (!err) {
+        return console.log('Server running')
+    }
+    console.log(err)
+})
